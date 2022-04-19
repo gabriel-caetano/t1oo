c9d23a7220242070d3303a8c0711bfd7f372dcaf
gabriel-caetano <gvcaetano@inf.ufsm.br>
2022-04-19 12:39:55 -0300

    nothing important

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-
         String url = "jdbc:mysql://localhost:12346/t1oo";
         Properties info = new Properties();
         info.put("user", "root");
         info.put("password", "123456789");
         System.out.println("Connecting database...");
-        try (Connection connection = DriverManager.getConnection(url, info)) {
+
+        try {
+            Connection connection = DriverManager.getConnection(url, info);
             System.out.println("Database connected!");
         } catch (SQLException e) {
-            System.out.print(e);
             throw new IllegalStateException("Cannot connect the database!", e);
         }
     }
